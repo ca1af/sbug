0964b856d8e0a969648f7c2217162944195dfa20
calaf <117057567+ca1af@users.noreply.github.com>
2023-02-07 14:47:21 +0900

    [feat] UserController 수정

--- src/main/java/com/sparta/sbug/user/controller/UserController.java
+++ src/main/java/com/sparta/sbug/user/controller/UserController.java
@@ -8,6 +8,7 @@ import com.sparta.sbug.user.entity.User;
 import com.sparta.sbug.user.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,11 @@ import java.util.List;
 public class UserController {
     private final UserServiceImpl userService;
     @RequestMapping("/api/user/sign-up")
-    public String signup(SignUpRequestDto requestDto){
+    public String signup(@RequestBody SignUpRequestDto requestDto){
         return userService.signup(requestDto);
     }
     @RequestMapping("/api/user/login")
-    public String login(LoginRequestDto requestDto){
+    public String login(@RequestBody LoginRequestDto requestDto){
         return userService.login(requestDto);
     }
     @RequestMapping("/api/user/unregister")
@@ -30,7 +31,7 @@ public class UserController {
         return userService.unregister(userDetails.getUser());
     }
     @RequestMapping("/api/user/update")
-    public String update(@AuthenticationPrincipal UserDetailsImpl userDetails, UserUpdateDto dto){
+    public String update(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody UserUpdateDto dto){
         userService.update(userDetails.getUser(),dto);
         return "updated";
     }
