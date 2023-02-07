144217aac272ff6264f4dcfe6454d31454b01065
calaf <117057567+ca1af@users.noreply.github.com>
2023-02-07 10:28:22 +0900

    .

--- src/main/java/com/sparta/sbug/schedule/entity/Schedule.java
+++ src/main/java/com/sparta/sbug/schedule/entity/Schedule.java
@@ -2,17 +2,14 @@ package com.sparta.sbug.schedule.entity;
 
 import com.sparta.sbug.common.entity.Timestamp;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-//import org.checkerframework.checker.units.qual.C;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "schedule")
 public class Schedule extends Timestamp {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
