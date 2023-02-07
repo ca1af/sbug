7443bc897fa061c0a084d8f34b36e53aafaa7d1c
calaf <117057567+ca1af@users.noreply.github.com>
2023-02-07 22:27:01 +0900

    [fix] ScheduleRepository 타입 문제 해결.

--- src/main/java/com/sparta/sbug/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/sbug/schedule/repository/ScheduleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ScheduleRepository extends JpaRepository {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
