b69bf812136ed13e95c6417364b1ba2c44b7f463
beom <baek940dog@gmail.com>
2023-02-08 14:09:55 +0900

    [Feat] Comment Controller 및 Comment Request DTO 추가

---
+++ src/main/java/com/sparta/sbug/comment/dto/CommentRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.sbug.comment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// lombok
+@Getter
+@NoArgsConstructor
+public class CommentRequestDto {
+    private String content;
+}
