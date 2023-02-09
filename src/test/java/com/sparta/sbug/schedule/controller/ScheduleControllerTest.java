293b3574881b433a423399f78e9bf91a1de123ea
youngfromnowhere <graph1000limited@gmail.com>
2023-02-10 02:13:26 +0900

    [Test] ScheduleController test code 일부 완성

--- src/test/java/com/sparta/sbug/schedule/controller/ScheduleControllerTest.java
+++ src/test/java/com/sparta/sbug/schedule/controller/ScheduleControllerTest.java
@@ -10,6 +10,7 @@ import com.sparta.sbug.security.userDetails.UserDetailsImpl;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +24,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doReturn;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
@@ -123,25 +125,49 @@ public class ScheduleControllerTest {
 
     }
 
-    /**
     @Test
     @DisplayName("getMySchedules() Controller Test")
     void getMySchedules() {
 
         //given
-        Page<ScheduleResponseDto> pageStub
-            = Page<ScheduleResponseDto>.empty();
+        Pageable pageableStub = PageRequest.of(3, 3);
+        User userStub = User.builder()
+            .email("123")
+            .password("123")
+            .nickname("123")
+            .build();
+        UserDetailsImpl userDetailsImplStub =
+            new UserDetailsImpl(userStub, "123");
 
-        //when
-        when(scheduleService.getMySchedules(
+        //비어있는 변수 resultStub을 만든다
+        Optional<Integer> resultStub = Optional.empty();
+
+        //Mock객체 scheduleService의 method는
+        //resultStub을 반환하도록 설정
+        //doReturn은 non-type safe라서 resultStub의 type을
+        //임의로 정할 수 있다.
+        doReturn(resultStub).when(scheduleService).getMySchedules(
             any(Pageable.class),
             any(User.class)
-        ))
-        .thenReturn(pageStub);
+        );
+        
+        //when
+
+        //scheduleController의 method(Test대상)의 return은
+        //response에 담는다.
+        Page<ScheduleResponseDto> response
+            = scheduleController.getMySchedules(
+                pageableStub,
+                userDetailsImplStub
+            );
 
         //then
+
+        //response와 resultStub의 주소가 같은지 확인
+        //이게 된다고..?
+        assertThat(response).isSameAs(resultStub);
+
     }
-    */
 
     @Test
     @DisplayName("getSchedule() Controller Test")
