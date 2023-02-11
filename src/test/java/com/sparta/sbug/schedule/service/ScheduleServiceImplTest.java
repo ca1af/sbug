b8087a7e36a591ca5711d29b562de6fd192012ee
youngfromnowhere <graph1000limited@gmail.com>
2023-02-11 20:52:59 +0900

    [Test] Test code on ScheduleServiceImpl

--- src/test/java/com/sparta/sbug/schedule/service/ScheduleServiceImplTest.java
+++ src/test/java/com/sparta/sbug/schedule/service/ScheduleServiceImplTest.java
@@ -13,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
@@ -35,8 +37,9 @@ public class ScheduleServiceImplTest {
 
     private ScheduleRequestDto request;
     private User user;
-    private long scheduleId;
     private long userId;
+    private Schedule schedule;
+    private long scheduleId;
 
     @BeforeEach
     public void init() {
@@ -52,8 +55,23 @@ public class ScheduleServiceImplTest {
             .nickname("123")
             .build();
 
-        scheduleId = 123L;
-        userId = 123L;
+        ReflectionTestUtils.setField(user, "id", 4878L);
+
+        userId = user.getId();
+
+        schedule = Schedule.builder()
+            .user(user)
+            .content("123")
+            .date(LocalDateTime.now())
+            .build();
+
+        ReflectionTestUtils.setField(schedule, "id", 4545L);
+
+        scheduleId = schedule.getId();
+
+        when(scheduleRepository.save(any(Schedule.class)))
+            .thenReturn(any(Schedule.class));
+
 
     }
 
@@ -61,10 +79,7 @@ public class ScheduleServiceImplTest {
     @DisplayName("ServiceImpl.registerSchedule Test")
     public void registerSchedule() {
         //given
-        //호출 여부만 검사
 
-        when(scheduleRepository.save(any(Schedule.class)))
-            .thenReturn(any(Schedule.class));
 
         //when
         scheduleServiceImpl.registerSchedule(request, user);
@@ -81,8 +96,8 @@ public class ScheduleServiceImplTest {
     public void updateSchedule() {
         //given
 
-        when(scheduleRepository.save(any(Schedule.class)))
-            .thenReturn(any(Schedule.class));
+        when(scheduleRepository.findById(scheduleId))
+            .thenReturn(Optional.ofNullable(schedule));
 
         //when
         scheduleServiceImpl.updateSchedule(
@@ -92,6 +107,11 @@ public class ScheduleServiceImplTest {
         );
 
         //then
+
+        verify(scheduleRepository, times(1)).findById(
+            any(Long.class)
+        );
+        
         verify(scheduleRepository, times(1)).save(
             any(Schedule.class)
         );
@@ -102,10 +122,22 @@ public class ScheduleServiceImplTest {
     public void deleteSchedule() {
         //given
 
+        when(scheduleRepository.findById(scheduleId))
+            .thenReturn(Optional.ofNullable(schedule));
         //when
+        scheduleServiceImpl.deleteSchedule(
+            scheduleId,
+            userId
+        );
 
         //then
-
+        verify(scheduleRepository, times(1)).findById(
+            any(Long.class)
+        );
+        
+        verify(scheduleRepository, times(1)).delete(
+            any(Schedule.class)
+        );
     }
 
     @Test
