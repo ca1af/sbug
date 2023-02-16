92996e58965b346ef4445e68652a227ea5ca473d
beom <baek940dog@gmail.com>
2023-02-16 10:34:07 +0900

    [Fix] Kakao Controller login RequestParam -> HttpServletRequest으로 변경

--- src/main/java/com/sparta/sbug/oauth2/controller/KakaoController.java
+++ src/main/java/com/sparta/sbug/oauth2/controller/KakaoController.java
@@ -3,9 +3,9 @@ package com.sparta.sbug.oauth2.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.sbug.oauth2.service.KakaoService;
 import com.sparta.sbug.security.dto.TokenResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +15,8 @@ public class KakaoController {
 
     //https://kauth.kakao.com/oauth/authorize?client_id=a6be9b62b761e5b5ee34bfa49d268617&redirect_uri=http://localhost:5500/login.html&response_type=code
     @GetMapping("/api/users/kakao")
-    public TokenResponseDto kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+    public TokenResponseDto kakaoLogin(HttpServletRequest request) throws JsonProcessingException {
+        var code = request.getHeader("code");
         return kakaoService.kakaoLogin(code);
     }
 }
