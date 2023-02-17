b9015bb1eed46b635cee6053bf760575d34574ac
calaf <117057567+ca1af@users.noreply.github.com>
2023-02-18 02:19:16 +0900

    [fix] 변수명 잘못된 부분 수정.

--- src/main/java/com/sparta/sbug/admin/AdminLoginController.java
+++ src/main/java/com/sparta/sbug/admin/AdminLoginController.java
@@ -4,21 +4,36 @@ import com.sparta.sbug.admin.dto.AdminResponseDto;
 import com.sparta.sbug.admin.service.AdminService;
 import com.sparta.sbug.security.dto.TokenResponseDto;
 import com.sparta.sbug.security.jwt.JwtProvider;
+import com.sparta.sbug.upperlayerservice.AdminUpperLayerService;
 import com.sparta.sbug.user.dto.LoginRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admins")
 public class AdminLoginController {
     private final AdminService adminService;
     private final JwtProvider jwtProvider;
+    private final AdminUpperLayerService adminUpperLayerService;
 
-    @PostMapping("/api/admins/login")
+    @PostMapping("/login")
     public TokenResponseDto adminLogin(@RequestBody LoginRequestDto requestDto){
         AdminResponseDto responseDto = adminService.adminLogin(requestDto);
         return jwtProvider.createTokenAdmin(responseDto.getEmail());
+    }
+
+    @PatchMapping("/comments/{commentId}")
+    public void disableComment(@PathVariable Long commentId){
+        adminUpperLayerService.disableComment(commentId);
+    }
+
+    @PatchMapping("/threads/{threadId}")
+    public void disableThread(@PathVariable Long threadId){
+        adminUpperLayerService.disableThread(threadId);
+    }
+    @PatchMapping("/channels/{channelId}")
+    public void disableChannel(@PathVariable Long channelId){
+        adminUpperLayerService.disableChannelAndDependentUserChannel(channelId);
     }
 }
