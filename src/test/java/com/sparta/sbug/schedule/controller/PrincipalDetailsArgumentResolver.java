27ebcf7505b9116e7f027432c38d5e159997d218
youngfromnowhere <graph1000limited@gmail.com>
2023-02-20 12:51:35 +0900

    임시 commit

---
+++ src/test/java/com/sparta/sbug/schedule/controller/PrincipalDetailsArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.sparta.sbug.schedule.controller;
+
+import com.sparta.sbug.security.userDetails.UserDetailsImpl;
+import com.sparta.sbug.user.entity.UserRole;
+import com.sparta.sbug.user.entity.User;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Collections;
+
+public class PrincipalDetailsArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().isAssignableFrom(UserDetailsImpl.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        User user = User.builder()
+            .email("some@somemail.com")
+            .password("1234qwer")
+            .nickname("bob")
+            .build();
+        ReflectionTestUtils.setField(user, "id", 4878L);
+
+        UserDetailsImpl userDetails = new UserDetailsImpl(user, user.getEmail());
+
+        return userDetails;
+    }
+
+
+
+
+}
--- src/test/java/com/sparta/sbug/schedule/controller/ScheduleControllerTest.java
+++ src/test/java/com/sparta/sbug/schedule/controller/ScheduleControllerTest.java
@@ -1,6 +1,9 @@
 package com.sparta.sbug.schedule.controller;
 
 import com.sparta.sbug.schedule.service.ScheduleService;
+import com.sparta.sbug.schedule.dto.ScheduleRequestDto;
+import com.sparta.sbug.security.userDetails.UserDetailsImpl;
+import com.sparta.sbug.user.entity.User;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Test;
@@ -10,12 +13,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
 
+import org.springframework.http.MediaType;
+
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -24,13 +31,23 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.BDDMockito.any;
 
+import java.time.LocalDateTime;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(ScheduleController.class)
 public class ScheduleControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mvc = MockMvcBuilders
+            .standaloneSetup(new ScheduleController())//여기 뭐가 들어가야 되는겨?
+            .setCustomArgumentResolvers(new PrincipalDetailsArgumentResolver())
+            .build();
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private ScheduleService scheduleService;
@@ -41,15 +58,19 @@ public class ScheduleControllerTest {
     @DisplayName("@PostMapping('') schedule() Test: registerSchedule")
     public void registerSchedule() {
         //given
-        LinkedMultiValueMap<String, String> requestParams =
-            new LinkedMultiValueMap<>();
-
-
         willDoNothing().given(scheduleService).registerSchedule(any(),any());
+
+        Map<String, String> input = new HashMap<>();
+        input.put("content", "Project meeting");
+        input.put("date", "2023-02-20 14:00:00");
 
 
         //when & then
-        mvc.perform(post("").param())
+        mvc.perform(post("")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(input))
+        )
+        .andExpect(status().isOk());
 
     }
     
