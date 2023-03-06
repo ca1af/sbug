6d106c955d45fce73ce37ee888ea61d9a0166e30
beom <baek940dog@gmail.com>
2023-03-07 01:26:27 +0900

    [Fix] Start.sh 버그 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 
-ps -ef | grep 'java -jar build/libs/sbug-0.0.1-SNAPSHOT.jar' | awk '{print $2}' | while read line; do kill $line; done
+ps -ef | grep 'java -jar build/libs/sbug-0.0.1-SNAPSHOT.jar' | awk '{print $2}' | while read line; do sudo kill $line; done
 
 nohup java -jar build/libs/*.jar > /dev/null 2> /dev/null < /dev/null &
